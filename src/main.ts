e4abf07d651799731a2b1b44454a1ffac94ab23e
Tuan Can <tuancv.dev@gmail.com>
2023-09-17 01:41:48 +0700

    Add common library for App

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as compression from 'compression';
+import helmet from 'helmet';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(compression());
+  app.use(helmet());
 
   const configService = app.get(ConfigService);
 
