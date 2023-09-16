b66dce5d4c0cb19ad225d4fbafbc3980661a9612
Tuan Can <tuancv.dev@gmail.com>
2023-09-17 00:41:23 +0700

    Apply nestjs configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './configs/configuration';
+import databaseConfig from './configs/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      load: [configuration, databaseConfig],
+      isGlobal: true,
+      cache: true,
+      expandVariables: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
